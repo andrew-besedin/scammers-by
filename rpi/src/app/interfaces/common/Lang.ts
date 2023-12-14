18080e5b5a87f160ffabf4132cb91d1b17a07e68
Andrew Besedin <110849995+andrew-besedin@users.noreply.github.com>
2023-12-14 23:36:22 +0300

    Delete rpi directory

--- rpi/src/app/interfaces/common/Lang.ts
+++
@@ -1,3 +0,0 @@
-type Lang = "ru" | "en";
-
-export default Lang;
--- rpi/src/components/UI/Preloader/Preloader.tsx
+++
@@ -1,9 +0,0 @@
-import styles from "./Preloader.module.scss";
-
-function Preloader() {
-    return (
-        <div className={styles["lds-spinner"]}><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-    )
-}
-
-export default Preloader;
